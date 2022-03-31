63d0d75fbc3b18a6a1851f49d42860aa7f622c19
PeterGeorgee <petergeoorge@gmail.com>
2022-03-31 14:28:05 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OurPageComponent } from './our-page/our-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OurPageComponent
   ],
   imports: [
     BrowserModule,
