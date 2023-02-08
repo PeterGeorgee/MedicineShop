7f0192c71dcc448797c0f1b221203096de314c5d
PeterGeorgee <petergeoorge@gmail.com>
2023-02-08 19:55:42 +0200

    [MODIFY] Lakeelsinore website and AlphaPeople + ico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,10 @@ export class AppComponent {
     { "name": "San Jacinto", "addressOne": "1477 S San Jacinto Ave.", "addressTwo": "San Jacinto, CA 92581", "website":"https://sanjacinto.medicineshoppe.com/", "photosrc":"./assets/san jacinto.jpg","disabled":false },
     { "name": "Palm Springs", "addressOne": "170 E Tahquitz Canyon Way", "addressTwo": "Palm Springs, CA 92262", "website":"https://palmsprings.medicineshoppe.com/", "photosrc":"./assets/palm springs.jpg","disabled":false },
     { "name": "Montclair", "addressOne": "9675 Monte Vista Ave", "addressTwo": "Montclair, CA", "website":"https://montclair.medicineshoppe.com/", "photosrc":"./assets/montclair.jpg","disabled":false },
-    { "name": "Lake Elsinore", "addressOne": "31946 Mission Trail Ste# A", "addressTwo": "Lake Elsinore, CA 92530", "website":"https://www.lakeelsinore.medicineshoppe.com/", "photosrc":"./assets/lake.jpg","disabled":false },
-    { "name": "Palm Desert California", "addressOne": "72960 Fred Waring Dr. Ste 11", "addressTwo": "Palm Desert, CA 92260", "website":"NO WEBSITE", "photosrc":"./assets/Palm springs 2.jpg","disabled":true },
+    { "name": "Lake Elsinore", "addressOne": "31946 Mission Trail Ste# A", "addressTwo": "Lake Elsinore, CA 92530", "website":"https://lakeelsinore.medicineshoppe.com/", "photosrc":"./assets/lake.jpg","disabled":false },
     { "name": "The Medicine Shoppe Retail Pharmacy", "addressOne": "5801 W Craig Road, Suite #110", "addressTwo": "Las Vegas, NV 89130", "website":"https://lasvegas.medicineshoppe.com/", "photosrc":"./assets/las vegas retail.jpg","disabled":false },
-    { "name": "Alpha Peoples Drugs", "addressOne": "1638 R Street, NW PH 1", "addressTwo": "Washington DC", "website":"NO WEBSITE", "photosrc":"./assets/AlphaPeoples.jpeg","disabled":true },
+    { "name": "Palm Desert California", "addressOne": "72960 Fred Waring Dr. Ste 11", "addressTwo": "Palm Desert, CA 92260", "website":"NO WEBSITE", "photosrc":"./assets/Palm springs 2.jpg","disabled":true },
+    { "name": "Alpha Peoples Drugs", "addressOne": "1638 R Street, NW PH 1", "addressTwo": "Washington DC", "website":"https://www.alphapeoplesdrugs.com/", "photosrc":"./assets/AlphaPeoples.jpeg","disabled":false },
     { "name": "Vida Drug Store", "addressOne": "437 N Euclid Ave suite B", "addressTwo": "Ontario, CA 91762", "website":"NO WEBSITE", "photosrc":"./assets/vida.jpg","disabled":true },
   ];
   lastBranch = { "name": "The Medicine Shoppe Long Term Care Pharmacy", "addressOne": "5801 W Craig Road, Suite #130", "addressTwo": "Las Vegas, NV 89130", "website":"NO WEBSITE", "photosrc":"./assets/MSImage2.jpg","disabled":true };
